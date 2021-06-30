0d1a8236e851d17e76542c34301993d47d6c4e15
Otávio Silva <otaviosilva2632@gmail.com>
2021-06-30 10:28:14 -0300

    feat: create category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Models;
+using testeef.Data;
+
+namespace testeef.Controllers
+{
+  [ApiController]
+  [Route("v1/categories")]
+  public class CategoryController : ControllerBase
+  {
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+    {
+      var categories = await context.Categories.ToListAsync();
+      return categories;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Category>> Post(
+      [FromServices] DataContext context, 
+      [FromBody] Category model)
+    {
+      if (ModelState.IsValid) 
+      {
+        context.Categories.Add(model);
+        await context.SaveChangesAsync();
+        return model;
+      }
+      
+      return BadRequest(ModelState);
+    }
+  }
+}
